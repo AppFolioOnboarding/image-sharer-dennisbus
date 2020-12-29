b82368e58d9590066d5955347dd499b102828040
Dennis Yeung <dennis.yeung@appfolio.com>
2020-12-28 18:48:51 -0800

    require image tag

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ApplicationRecord
   URL_REGEX = %r{\A(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}i.freeze
   acts_as_taggable_on :tags
+  validates :tag_list, presence: true
   validates :name, presence: true
   validates :url, presence: true, format: { with: URL_REGEX, message: 'please enter a valid HTTP url path' }
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -12,14 +12,15 @@ class ImageTest < ActiveSupport::TestCase
     image = Image.new
     image.url = 'http://www.google.com/12345.png'
     image.save
-    assert_equal(image.errors.full_messages, ["Name can't be blank"])
+    assert_includes(image.errors.full_messages, "Name can't be blank")
   end
 
   test 'should not save image without url' do
     image = Image.new
     image.name = 'fav img'
     assert_not image.save
-    assert_equal(image.errors.full_messages, ["Url can't be blank", url_error_mesg])
+    assert_includes(image.errors.full_messages, "Url can't be blank")
+    assert_includes(image.errors.full_messages, url_error_mesg)
   end
 
   test 'should not save image without valid url' do
@@ -27,7 +28,7 @@ class ImageTest < ActiveSupport::TestCase
     image.name = 'fav img'
     image.url = 'invalid url path'
     assert_not image.save
-    assert_equal(image.errors.full_messages, [url_error_mesg])
+    assert_includes(image.errors.full_messages, url_error_mesg)
   end
 
   test 'should not save image without http url' do
@@ -35,7 +36,7 @@ class ImageTest < ActiveSupport::TestCase
     image.name = 'fav img'
     image.url = 'www.google.com/12345.png'
     assert_not image.save
-    assert_equal(image.errors.full_messages, [url_error_mesg])
+    assert_includes(image.errors.full_messages, url_error_mesg)
   end
 
   test 'save image successfully' do
@@ -45,6 +46,7 @@ class ImageTest < ActiveSupport::TestCase
     image.name = img_name
     img_url = 'http://www.google.com/12345.png'
     image.url = img_url
+    image.tag_list = 'test'
     assert image.save
     assert_empty(image.errors.full_messages) # no error mesg
 
@@ -53,7 +55,15 @@ class ImageTest < ActiveSupport::TestCase
     assert_not_nil(Image.find(image.id))
     assert_equal(Image.find(image.id).name, img_name)
     assert_equal(Image.find(image.id).url, img_url)
-    assert_empty(Image.find(image.id).tag_list)
+    assert_equal(Image.find(image.id).tag_list.count, 1)
+  end
+
+  test 'should not save image without tag' do
+    image = Image.new
+    image.name = 'fav img'
+    image.url = 'http://www.google.com/12345.png'
+    assert_not image.save
+    assert_includes(image.errors.full_messages, "Tag list can't be blank")
   end
 
   test 'save image with tags' do
